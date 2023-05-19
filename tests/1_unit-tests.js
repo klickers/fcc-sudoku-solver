4161dbda66084ddc102063ca66ffa812989c135d
klickers <bluebirid228@gmail.com>
2023-05-19 12:14:36 -0700

    update test "Solver returns the expected solution for an incomplete puzzle"

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,8 @@ const assert = chai.assert;
 const Solver = require("../controllers/sudoku-solver.js");
 let solver = new Solver();
 
+const puzzlesAndSolutions = require("../controllers/puzzle-strings.js");
+
 suite("Unit Tests", () => {
     test("Logic handles a valid puzzle string of 81 characters", function (done) {
         assert.equal(
@@ -144,29 +146,8 @@ suite("Unit Tests", () => {
     });
 
     test("Solver returns the expected solution for an incomplete puzzle", function (done) {
-        assert.equal(
-            solver.solve(
-                "5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3"
-            ),
-            "568913724342687519197254386685479231219538467734162895926345178473891652851726943"
-        );
-        assert.equal(
-            solver.solve(
-                "..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1"
-            ),
-            "218396745753284196496157832531672984649831257827549613962415378185763429374928561"
-        );
-        assert.equal(
-            solver.solve(
-                ".7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6"
-            ),
-            "473891265851726394926345817568913472342687951197254638734162589685479123219538746"
-        );
-        assert.equal(
-            solver.solve(
-                "82..4..6...16..89...98315.749.157.............53..4...96.415..81..7632..3...28.51"
-            ),
-            "827549163531672894649831527496157382218396475753284916962415738185763249374928651"
+        puzzlesAndSolutions.puzzlesAndSolutions.forEach((puzzle) =>
+            assert.equal(solver.solve(puzzle[0]), puzzle[1])
         );
         done();
     });
