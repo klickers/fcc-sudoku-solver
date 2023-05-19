5cf928a4f21d8920e38ba5b26c65e0a4cd37c808
klickers <bluebirid228@gmail.com>
2023-05-19 10:27:31 -0700

    add .prettierrc

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,13 @@
 class SudokuSolver {
+    validate(puzzleString) {}
 
-  validate(puzzleString) {
-  }
+    checkRowPlacement(puzzleString, row, column, value) {}
 
-  checkRowPlacement(puzzleString, row, column, value) {
+    checkColPlacement(puzzleString, row, column, value) {}
 
-  }
+    checkRegionPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
-
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+    solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
